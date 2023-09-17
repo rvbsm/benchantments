4ab2e7d611bc7e10bfc4e62db5bd79b2ad7e4f6b
Ruslan Mykhailenko <mihailencko.r222@gmail.com>
2023-09-18 00:38:54 +0300

    refactor: change MOD_ID

--- src/main/java/dev/rvbsm/benchantments/BEnchantmentsMod.java
+++ src/main/java/dev/rvbsm/benchantments/BEnchantmentsMod.java
@@ -7,7 +7,7 @@ import java.nio.file.Path;
 
 public class BEnchantmentsMod implements ModInitializer {
 
-	private static final String MOD_ID = "better-enchantments";
+	private static final String MOD_ID = "benchantments";
 	private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID + ".toml");
 	public static final ModConfig CONFIG = new ModConfig(CONFIG_PATH);
 
